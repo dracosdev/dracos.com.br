e9159a18c94d2e657e2aa796ddc3681a509b7139
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-02-07 19:44:33 -0400

    Adicionando telas do jogo
    Adicionado comportamento de clicar para iniciar o jogo;

--- game/jogo.js
+++ game/jogo.js
@@ -105,16 +105,52 @@ const mensagemGetReady={
   }
 }
 
+let telaAtiva={};
+function mudaParaTela(novaTela){
+  telaAtiva=novaTela;
+}
+
+const Telas={
+  INICIO:{
+    desenha(){
+      planoDeFundo.desenha();
+      chao.desenha();
+      flappyBird.desenha();
+      mensagemGetReady.desenha();
+    },
+
+    click(){
+      mudaParaTela(Telas.JOGO);
+    },
+    atualiza(){
+
+    }
+  }
+};
+
+Telas.JOGO={
+  desenha(){
+    planoDeFundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+  },
+  atualiza(){
+    flappyBird.atualiza();
+  }
+};
+
 function loop() {
-
-  flappyBird.atualiza();
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
-
-  mensagemGetReady.desenha();
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
 
   requestAnimationFrame(loop);
 }
 
+
+window.addEventListener('click', function(){
+  if(telaAtiva.click){
+    telaAtiva.click();
+  }
+})
+mudaParaTela(Telas.INICIO);
 loop();

ecccdd7eb5a0e2c39822ffb6447723c55c138e01
Diogo Fagundes <diogo.fagundes@calindra.com.br>
2022-03-28 22:17:05 -0300

    Remoção de rotas não utilizadas

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ app.get("/ame", function(req, res){
 	res.sendFile(__dirname + "/ame/index.html");
 });
 
-app.get("/ongs", function(req, res){
-	res.sendFile(__dirname + "/ame/ongs.json");
-});
-
-app.get("/ongsdl", function(req, res){
-	res.sendFile(__dirname + "/ame/ongsdownload.js");
-});
-
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });

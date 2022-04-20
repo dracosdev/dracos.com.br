3a701eb2f25da419b3b27da73a8de8936aef3fcf
Diogo Fagundes <diogo.fagundes@calindra.com.br>
2022-04-20 13:39:16 -0300

    Remoção de arquivo não utilizado

--- ame/config.js
+++
@@ -1,3 +0,0 @@
-// const devUrl = 'https://miniapps.dev.amedigital.com/miniapp-manager-api/o/mini-apps/'
-// const hmlUrl = 'https://miniapps.hml.amedigital.com/miniapp-manager-api/o/mini-apps/'
-// const prdUrl = 'https://miniapps.amedigital.com/miniapp-manager-api/o/mini-apps/'
